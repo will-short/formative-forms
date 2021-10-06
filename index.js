200c15499975a4897ff0baf24e6dbbb8b17bad80
will short <willofmetal85@gmail.com>
2021-10-06 17:06:53 -0500

    hello

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const csrfProtection = csurf({ cookie: true });
 
 app.use(cookieParser());
 
+app.use(express.urlencoded({ extended: false }));
+
 const users = [
   {
     id: 1,
@@ -26,8 +28,64 @@ app.get("/", (req, res) => {
 });
 
 app.get("/create", csrfProtection, function (req, res) {
-  // pass the csrfToken to the view
-  res.render("create", { csrfToken: req.csrfToken() });
+  let errors = [];
+  res.render("create", { csrfToken: req.csrfToken(), errors });
+});
+
+app.post("/create", csrfProtection, (req, res) => {
+  let form = req.body;
+  let errors = [];
+
+  if (!form.firstName) errors.push("Please provide a first name.");
+  if (!form.lastName) errors.push("Please provide a last name.");
+  if (!form.email) errors.push("Please provide an email.");
+  if (!form.password) errors.push("Please provide a password.");
+  if (form.password !== form.confirmedPassword)
+    errors.push(
+      "The provided values for the password and password confirmation fields did not match."
+    );
+  if (errors.length)
+    res.render("create", { csrfToken: req.csrfToken(), errors, form });
+  else {
+    users.push({
+      id: users[users.length - 1].id + 1,
+      firstName: form.firstName,
+      lastName: form.lastName,
+      email: form.email,
+    });
+    res.redirect("/");
+  }
+});
+
+app.get("/create-interesting", csrfProtection, function (req, res) {
+  let errors = [];
+  res.render("create-interesting", { csrfToken: req.csrfToken(), errors });
+});
+
+app.post("/create-interesting", csrfProtection, (req, res) => {
+  let form = req.body;
+  let errors = [];
+
+  if (!form.firstName) errors.push("Please provide a first name.");
+  if (!form.lastName) errors.push("Please provide a last name.");
+  if (!form.email) errors.push("Please provide an email.");
+  if (!form.password) errors.push("Please provide a password.");
+  if (!form.password) errors.push("Please provide a password.");
+  if (form.password !== form.confirmedPassword)
+    errors.push(
+      "The provided values for the password and password confirmation fields did not match."
+    );
+  if (errors.length)
+    res.render("create", { csrfToken: req.csrfToken(), errors, form });
+  else {
+    users.push({
+      id: users.length + 1,
+      firstName: form.firstName,
+      lastName: form.lastName,
+      email: form.email,
+    });
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
