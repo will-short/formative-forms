e5ce90a18d1f8a6d4c5a89d7cf4afec692f496c3
will short <willofmetal85@gmail.com>
2021-10-06 15:00:05 -0500

    phase1-2 solved

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const express = require("express");
 
 const app = express();
+const csurf = require("csurf");
+const cookieParser = require("cookie-parser");
+
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const csrfProtection = csurf({ cookie: true });
+
+app.use(cookieParser());
 
 const users = [
   {
     id: 1,
     firstName: "Jill",
     lastName: "Jack",
-    email: "jill.jack@gmail.com"
-  }
+    email: "jill.jack@gmail.com",
+  },
 ];
+
+app.get("/", (req, res) => {
+  res.render("index", { users });
+});
+
+app.get("/create", csrfProtection, function (req, res) {
+  // pass the csrfToken to the view
+  res.render("create", { csrfToken: req.csrfToken() });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
